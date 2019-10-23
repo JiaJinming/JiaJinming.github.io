8a3628caa70faf0d08a66398239fa50fcaf016aa
JM <772972603@qq.com>
2019-10-23 22:22:42 +0800

    网页

---
+++ js/my.js
@@ -0,0 +1,7 @@
+function topInformation(type, value) {
+    if(type == 1)  alert("请加QQ  " + value);
+    if(type == 2)  alert("请加微信  JJM" + value);
+    if(type == 3)  alert("请加Facebook账号  " + value);
+    if(type == 4)  alert("请拨打电话  " + value);
+    if(type == 5)  alert(value);
+    }
